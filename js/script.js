cf9b8f01eb4e9c3469d694db1b2bcc7ff4f8444d
David Calabrese <david.thomas.calabrese@gmail.com>
2021-03-06 20:35:44 -0600

    background color changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 let poems = document.querySelectorAll(".flex-item");
 
+const randColor = () => {
+    let hexColor = Math.floor(Math.random() * 16777215).toString(16);
+    return hexColor;
+};
+
 poems.forEach((poem) => {
   poem.addEventListener("mouseover", (e) => {
     console.log(e);
@@ -7,6 +12,14 @@ poems.forEach((poem) => {
   });
 
   poem.addEventListener("mouseleave", () => {
-      poem.style.outline = ""
-  })
+    poem.style.outline = "";
+  });
+
+  poem.addEventListener("click", () => {
+    if (poem.style.backgroundColor) {
+      poem.style.backgroundColor = "";
+    } else {
+      poem.style.backgroundColor = `#${randColor()}`;
+    }
+  });
 });
